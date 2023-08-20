393e86245ca2ebfefdc6bcc52cdce7c082f92d8c
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2023-08-20 04:11:17 +0300

    sloving some error & new feature

--- Automated EDA.py
+++ Automated EDA.py
@@ -1,10 +1,12 @@
 import os
 import pandas as pd
-import seaborn as sns
+import seaborn 
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 import random
 
+
+######### this function to read data using path of data in your computer ##########
 def loadDataFromPC(path):
     try:
         _, file_extension = os.path.splitext(path)
@@ -26,27 +28,22 @@ def loadDataFromPC(path):
 
 
 
-
+######### this function to handeling data and make it able to be readable ##########
 def handeling_data(data):
-    categorical_columns = data.select_dtypes(include="object").columns
-    numerical_columns = data.select_dtypes(include=["int", "float"]).columns
-    data[numerical_columns] = data[numerical_columns].fillna(data[numerical_columns].mean())
-    data[categorical_columns] = data[categorical_columns].fillna(data[categorical_columns].mode().iloc[0])
-
-
-    encoded_data = pd.get_dummies(data, columns=categorical_columns, drop_first=True)
-
+    categoricalColumns = data.select_dtypes(include="object").columns
+    numericalColumns = data.select_dtypes(include=["int", "float"]).columns
+    data[numericalColumns] = data[numericalColumns].fillna(data[numericalColumns].mean())
+    data[categoricalColumns] = data[categoricalColumns].fillna(data[categoricalColumns].mode().iloc[0])
+    encoded_data = pd.get_dummies(data, columns=categoricalColumns, drop_first=True)
     scaler = StandardScaler()
-    scaled_numerical = scaler.fit_transform(data[numerical_columns])
-    encoded_data[numerical_columns] = scaled_numerical
-
+    scaledNumerical = scaler.fit_transform(data[numericalColumns])
+    encoded_data[numericalColumns] = scaledNumerical
     encoded_data.columns = encoded_data.columns.str.replace("_", "")
 
     return encoded_data
 
 
-
-
+######### this function to display Histogram  ##########
 def displayHistogram(data, colTitleName):
     plt.figure(figsize=(12, 8))
 
@@ -57,44 +54,43 @@ def displayHistogram(data, colTitleName):
            ## like 'str' data
             value_counts = column_data.value_counts()
 
-            sns.barplot(x=value_counts.index, y=value_counts.values)
+            seaborn.barplot(x=value_counts.index, y=value_counts.values)
             plt.xlabel(colTitleName)
 
             for i, count in enumerate(value_counts.values):
                 plt.text(i, count, str(count), ha="center", va="bottom")
             plt.show()
         else:
-            sns.histplot(
-                column_data, kde=True, color=random.choice(list(sns.color_palette()))
+            seaborn.histplot(
+                column_data, kde=True, color=random.choice(list(seaborn.color_palette()))
             )
             plt.xlabel(colTitleName)
             plt.show()
     else:
         print(f"Column '{colTitleName}' does not exist in the data.")
+ 
 
-
+######### this function to display Boxplot  ##########
 def displayBoxplot(data, posOfxColumn, posOfyColunm):
     name = "myBoxplot"
 
-    plt.figure(figsize=(8, 6))
     if data[posOfxColumn].dtype == "object":
         unique_x = data[posOfxColumn].unique()
-        
+        seaborn.boxplot(data=data, x=posOfxColumn, y=posOfyColunm)
         plt.title(f"comparing {posOfxColumn} vs {posOfyColunm} {name}")
         plt.xlabel(posOfxColumn)
         plt.ylabel(posOfyColunm)
 
     elif data[posOfyColunm].dtype == "object":
-        unique_y = data[posOfyColunm].unique()
-        colors = random.choices(list(sns.color_palette()), k=len(unique_y))
-
-    
-        plt.title(f"{posOfxColumn} vs {posOfyColunm} {name}")
+        unique_x = data[posOfxColumn].unique()
+        colors = random.choices(list(seaborn.color_palette()), k=len(unique_x))
+        seaborn.scatterplot(data=data, x=posOfxColumn, y=posOfyColunm, hue=posOfxColumn,palette=colors)
+        plt.title(f"comparing {posOfxColumn} vs {posOfyColunm} {name}")
         plt.xlabel(posOfxColumn)
         plt.ylabel(posOfyColunm)
 
     else:
-        
+        seaborn.boxplot(data=data, x=posOfxColumn, y=posOfyColunm)
         plt.title(f"{posOfxColumn} vs {posOfyColunm} {name}")
         plt.xlabel(posOfxColumn)
         plt.ylabel(posOfyColunm)
@@ -102,50 +98,79 @@ def displayBoxplot(data, posOfxColumn, posOfyColunm):
     plt.tight_layout()
     plt.show()
 
-        
+######### this function to display Scatterplot  ##########        
 def displayingScatterplot(data, posOfxColumn, posOfyColunm):
     name = "myScatterplot"
-    plt.figure(figsize=(8, 6))
+
 
     if data[posOfxColumn].dtype == "object" and data[posOfyColunm].dtype == "object":
         unique_x = data[posOfxColumn].unique()
-        colors = random.choices(list(sns.color_palette()), k=len(unique_x))
-
-        sns.scatterplot(data=data, x=posOfxColumn, y=posOfyColunm, hue=posOfxColumn, palette=colors)
+        colors = random.choices(list(seaborn.color_palette()), k=len(unique_x))
+        seaborn.scatterplot(data=data, x=posOfxColumn, y=posOfyColunm, hue=posOfxColumn,palette=colors)
 
     elif data[posOfxColumn].dtype == "object":
         unique_x = data[posOfxColumn].unique()
-        colors = random.choices(list(sns.color_palette()), k=len(unique_x))
-
-        sns.stripplot(data=data, x=posOfxColumn, y=posOfyColunm, hue=posOfxColumn, palette=colors)
+        colors = random.choices(list(seaborn.color_palette()), k=len(unique_x))
+        seaborn.stripplot(data=data, x=posOfxColumn, y=posOfyColunm, hue=posOfxColumn,palette=colors)
 
     elif data[posOfyColunm].dtype == "object":
         unique_y = data[posOfyColunm].unique()
-        colors = random.choices(list(sns.color_palette()), k=len(unique_y))
+        seaborn.stripplot(data=data, x=posOfxColumn, y=posOfyColunm, hue=posOfyColunm,palette=colors)
+        colors = random.choices(list(seaborn.color_palette()), k=len(unique_y))
         
 
     else:
-        
-        plt.title(f"comparing {posOfxColumn} vs {posOfyColunm} {name}")
-        plt.xlabel(posOfxColumn)
-        plt.ylabel(posOfyColunm)
-        plt.tight_layout()
-        plt.show()
+        seaborn.scatterplot(data=data, x=posOfxColumn, y=posOfyColunm)
+    plt.title(f"comparing {posOfxColumn} vs {posOfyColunm} {name}")
+    plt.xlabel(posOfxColumn)
+    plt.ylabel(posOfyColunm)
+    plt.tight_layout()
+    plt.show()
 
-
+######### this function to display Piechart  ##########
 def displayingPiechart(data, column):
     name = "Piechart"
     value_counts = data[column].value_counts()
     labels = value_counts.index
     counts = value_counts.values
 
-    plt.figure(figsize=(8, 6))
-    plt.pie(counts, labels=labels,colors=sns.color_palette('Set3'))
+    plt.figure(figsize=(10, 6))
+    plt.pie(counts, labels=labels,colors=seaborn.color_palette('Set3'))
     plt.title(f"{column} {name}")
     plt.axis('equal')
     plt.show()
 
-        
+######### this function to display PlotingData  ##########
+def displayPlotingData(data, h1, h2, h3):
+    try:
+        if h1 not in data.columns or h2 not in data.columns or h3 not in data.columns:
+            raise ValueError("One or more of the specified headers are not present in the data.")
+
+        if data[h1].dtype != 'object' or data[h2].dtype != 'object' or data[h3].dtype != 'object':
+            raise ValueError("All three headers must contain categorical data.")
+
+        value_counts_h1 = data[h1].value_counts()
+        value_counts_h2 = data[h2].value_counts()
+        value_counts_h3 = data[h3].value_counts()
+
+        plt.figure(figsize=(10, 6))
+
+        seaborn.lineplot(data=value_counts_h1, x=value_counts_h1.index, y=value_counts_h1.values, label=h1)
+        seaborn.lineplot(data=value_counts_h2, x=value_counts_h2.index, y=value_counts_h2.values, label=h2)
+        seaborn.lineplot(data=value_counts_h3, x=value_counts_h3.index, y=value_counts_h3.values, label=h3)
+
+        plt.legend()
+        plt.xlabel("Unique Values")
+        plt.ylabel("Counts")
+        plt.title(f'Value Counts of {h1}, {h2}, and {h3}')
+        plt.show()
+
+    except ValueError as ve:
+        print(f"ValueError: {ve}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
 def main():
     try:
         file_path = input("Give me your path to display your data: ")
@@ -156,9 +181,11 @@ def main():
             print("Press 2. To display Box Plot")
             print("Press 3. To display Scatter Plot")
             print("Press 4. To display Pie Chart")
-            print("Press 5. To Leave")
+            print("Press 5. To displayPlotingData") 
+            print("Press 6. To Leave")
+            
+            userChoice = int(input("Enter your choice (1/2/3/4/5/6): "))
 
-            userChoice = int(input("Enter your choice (1/2/3/4/5): "))
 
             if userChoice == 1:
                 data = loadDataFromPC(file_path)
@@ -181,6 +208,7 @@ def main():
                 column_choice = int(input("Enter the number of column you want: "))
                 if 0 <= column_choice < len(data.columns):
                     column = data.columns[column_choice]
+                    print('\n\nplease waiting .......\n\n')
                     displayHistogram(data, column)
                 else:
                     print("Invalid choice. Please try again.\n")
@@ -210,6 +238,7 @@ def main():
                 if 0 <= x_column_choice < len(data.columns) and 0 <= y_column_choice < len(data.columns):
                         posOfxColumn = data.columns[x_column_choice]
                         posOfyColunm = data.columns[y_column_choice]
+                        print('\n\nplease waiting .......\n\n')
                         displayBoxplot(data, posOfxColumn, posOfyColunm)
                 else:
                         print("Invalid choice. Please try again.")
@@ -234,6 +263,7 @@ def main():
                 if 0 <= x_column_choice < len(data.columns) and 0 <= y_column_choice < len(data.columns):
                     posOfxColumn = data.columns[x_column_choice]
                     posOfyColunm = data.columns[y_column_choice]
+                    print('\n\nplease waiting .......\n\n')
                     displayingScatterplot(data, posOfxColumn, posOfyColunm)
                 else:
                     print("Invalid choice. Please try again.")
@@ -259,11 +289,29 @@ def main():
                 column_choice = int(input("Enter the number of column you want: "))
                 if 0 <= column_choice < len(data.columns):
                     column = data.columns[column_choice]
+                    print('\n\nplease waiting .......\n\n')
                     displayingPiechart(data, column)
                 else:
                     print("Invalid choice. Please try again.")
 
             elif userChoice == 5:
+                 data = loadDataFromPC(file_path)
+                 dataAfterProcessing = handeling_data(data)
+                 header_names = data.columns.tolist()
+                 print("Available Headers:")
+                 for i, header in enumerate(header_names):
+                    print(f"{i}. {header}")
+
+                 hed1 = int(input("Give me the number of header1 column: "))
+                 hed2 = int(input("Give me the number of header2 column: "))
+                 hed3 = int(input("Give me the number of header3 column: "))
+                 if 0 <= hed1 < len(data.columns) and 0 <= hed2 < len(data.columns) and 0 <= hed3 < len(data.columns):
+                    headerOneData = data.columns[hed1]
+                    headerTwoData = data.columns[hed2]
+                    headerThreeData = data.columns[hed3]
+                    print('\n\nplease waiting .......\n\n')
+                    displayPlotingData(dataAfterProcessing, headerOneData, headerTwoData,headerThreeData)
+            elif userChoice == 6:
                 break
             else:
                 print("Invalid choice. Please try again.")
